ca1a07c1f4aabb92752b8bd9c0a211c090499052
Freites Medina <O000872@bf.arg.igrupobbva>
2022-08-05 10:46:55 -0300

    Initial commit

---
+++ src/main/java/com/bbva/apimoviespring/ApiMovieSpringApplication.java
@@ -0,0 +1,13 @@
+package com.bbva.apimoviespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMovieSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMovieSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bbva/apimoviespring/ApiMovieSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbva.apimoviespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMovieSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
